9fc599b8d0645f392e3e0122fbd4f6f62d5edd7e
whokilleddb <whokilleddb@gmail.com>
2022-02-23 11:44:57 +0530

    QEMU Emulation Script

---
+++ qemu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+cargo build
+qemu-system-x86_64  \
+    -enable-kvm \
+    -m 128 \
+    -nographic \
+    -bios /usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
+    -device driver=e1000,netdev=n0 \
+    -netdev user,id=n0,tftp=target/x86_64-unknown-uefi/debug,bootfile=lazarus.efi
